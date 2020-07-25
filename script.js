c89f69f561e67d2b7d5089593479289065aa67ab
nahid <nahidkishore99@gmail.com>
2020-07-25 21:17:58 +0600

    test

---
+++ script.js
@@ -0,0 +1,12 @@
+const calculate =document.getElementById('addNumber');
+calculate.addEventListener('click', function (){
+ 
+const numberInput=document.getElementById('bangla-number').value;
+const numberCount=parseFloat(numberInput);
+
+
+ document.getElementById('grade').innerText;
+ let numberCount=0;
+ if()
+
+})

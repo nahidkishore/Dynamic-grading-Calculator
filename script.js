844f1320b8bc84f262b2c1163bcd65ba87608d97
nahid <nahidkishore99@gmail.com>
2020-07-26 01:23:45 +0600

    english grade calculated

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 
+/* 
+function getInputNumber(id){
+
+}
+ */
+
+
+
+
+
+
+
+
 // Bangla Subject calculation
 
          const calculate =document.getElementById('addNumber');
@@ -6,13 +19,12 @@
           
           const numberInput=document.getElementById('bangla-number').value;
           const numberCount=parseFloat(numberInput);
-
-          /* const grade=document.getElementById('grade').innerText
-          const gradeNumber=parseFloat(grade); */
+       
   
-   /* let numberCount=0; */
-                        if(numberCount>=90 && numberCount<=93){
+ 
+                         if(numberCount>=90 && numberCount<=93){
                           document.getElementById('grade').innerText= "A";
+                         
 
                         }
                         else if(numberCount>=87 && numberCount<=89){
@@ -20,11 +32,11 @@
 
                         }
                         else if(numberCount>=83 && numberCount<=86){
-                          document.getElementById('grade').innerText= "B+";
+                           document.getElementById('grade').innerText= "B+";
 
                         }
                         else if(numberCount>=80 && numberCount<=82){
-                          document.getElementById('grade').innerText= "B";
+                           document.getElementById('grade').innerText= "B";
 
                         }
                         else if(numberCount>=77 && numberCount<=79){
@@ -40,7 +52,7 @@
 
                         }
                         else if(numberCount>=67 && numberCount<=69){
-                          document.getElementById('grade').innerText= "C-";
+                           document.getElementById('grade').innerText= "C-";
 
                         }
                         else if(numberCount>=63 && numberCount<=66){
@@ -52,17 +64,100 @@
 
                         }
                         else{
-                          document.getElementById('grade').innerText='F'
+                         document.getElementById('grade').innerText='F'
                         }
 
 
 
-                        const Marks=document.getElementById('total-marks').innerText;
-                        const marksCount=parseFloat(Marks);
+                        const marks=document.getElementById('total-marks').innerText;
+                        const marksCount=parseFloat(marks);
                         const totalMarks=numberCount+ marksCount;
                         document.getElementById('total-marks').innerText=totalMarks;
 
+                      
 
+                       // addCalculator("bangla-number");
+                      
+                       document.getElementById("bangla-number").value="";
 
-                  document.getElementById('bangla-number').value="";
 })
+
+
+// English subject
+
+const calculateEnglish =document.getElementById('addNUmberEnglish');
+calculateEnglish.addEventListener('click', function (){
+
+const numberInput=document.getElementById('english-number').value;
+const numberCount=parseFloat(numberInput);
+
+
+
+               if(numberCount>=90 && numberCount<=93){
+                document.getElementById('grade-english').innerText= "A";
+               
+
+              }
+              else if(numberCount>=87 && numberCount<=89){
+                document.getElementById('grade-english').innerText= "A-";
+
+              }
+              else if(numberCount>=83 && numberCount<=86){
+                 document.getElementById('grade-english').innerText= "B+";
+
+              }
+              else if(numberCount>=80 && numberCount<=82){
+                 document.getElementById('grade-english').innerText= "B";
+
+              }
+              else if(numberCount>=77 && numberCount<=79){
+                document.getElementById('grade-english').innerText= "B-";
+
+              }
+              else if(numberCount>=73 && numberCount<=76){
+                document.getElementById('grade-english').innerText= "C+";
+
+              }
+              else if(numberCount>=70 && numberCount<=72){
+                document.getElementById('grade-english').innerText= "C";
+
+              }
+              else if(numberCount>=67 && numberCount<=69){
+                 document.getElementById('grade-english').innerText= "C-";
+
+              }
+              else if(numberCount>=63 && numberCount<=66){
+                document.getElementById('grade-english').innerText= "D+";
+
+              }
+              else if(numberCount>=60 && numberCount<=62){
+                document.getElementById('grade-english').innerText= "D";
+
+              }
+              else{
+               document.getElementById('grade-english').innerText='F'
+              }
+
+
+
+              const marks=document.getElementById('total-marks').innerText;
+              const marksCount=parseFloat(marks);
+              const totalMarks=numberCount+ marksCount;
+              document.getElementById('total-marks').innerText=totalMarks;
+
+            
+
+             // addCalculator("bangla-number");
+            
+             document.getElementById("english-number").value="";
+
+})
+
+
+/* const calculatedEnglish=document.getElementById('addNUmberEnglish');
+calculatedEnglish.addEventListener('click', function(){
+
+  addCalculator('english-number');
+  document.getElementById('english-number').value="";
+})
+ */
